6f27ee855cce54cb593812870ff547ef64c54fdb
chiakisasa <103346152+chiakisasa@users.noreply.github.com>
2022-07-27 12:56:38 +0800

    Add files via upload

---
+++ src/main/java/com/example/traceability_platform/mapper/ChainMapper.java
@@ -0,0 +1,10 @@
+package com.example.traceability_platform.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.traceability_platform.entity.Chaindata;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChainMapper extends BaseMapper<Chaindata> {
+
+}
